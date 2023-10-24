1797e60e70d0170f78d5d234135843799b84f5cc
chilaust <118034353+chilaust@users.noreply.github.com>
2023-10-24 13:01:21 -0600

    Changed longest_line.cpp should be working now

--- Unit01-Intro-to-cpp/homework1b-file-io/longest_line.cpp
+++ Unit01-Intro-to-cpp/homework1b-file-io/longest_line.cpp
@@ -1,8 +1,62 @@
 #include <iostream>
+using std::cout, std::cerr, std::endl;
+
+#include <fstream>
+using std::ifstream, std::ofstream;
+
 #include <string>
+using std::string, std::getline;
+
+#include <sstream>
+using std::istringstream;
+
+int test_files(int argc, char const* argv[]) {
+    // First arg is input file, second arg is output file,
+    if (argc < 2) {
+        cerr << "This program needs an input file!" << endl;
+        exit(1);
+    }
+
+    ifstream infile(argv[1]);
+    if (!infile.is_open()) {
+        cerr << "Unable to open file for reading: " << argv[1] << endl;
+        exit(2);
+    }
+    return 0;
+}
 
 int main(int argc, char const* argv[]) {
-    // Write your code here
 
+    test_files(argc, argv);
+
+    ifstream infile(argv[1]);
+
+    string line;
+    string winner;
+    int length = 0;
+    while (getline(infile, line)) {
+        istringstream iss(line);
+        string word;
+        int count = 0;
+//        cout << line;
+//        cout << iss;
+
+        while (iss >> word) {
+            count ++;
+//            cout << count << "--count loop 1--" << endl;
+        }
+
+        if (count > length) {
+            winner = line;
+            length = count;
+//            cout << winner << "--winner--" << endl;
+//            cout << length << "--length loop 2--" << endl;
+        }
+
+    }
+
+    cout << winner << endl;
+
+    infile.close();
     return 0;
 }
--- Unit01-Intro-to-cpp/homework1b-file-io/reformat.cpp
+++ Unit01-Intro-to-cpp/homework1b-file-io/reformat.cpp
@@ -7,9 +7,58 @@ using std::ifstream, std::ofstream;
 #include <string>
 using std::string, std::getline;
 
+#include <sstream>
+using std::istringstream;
+
 
 int main(int argc, char const* argv[]) {
-    // Write your code here
+    // First arg is input file, second arg is output file,
+    if (argc < 3) {
+        cerr << "This program needs an input file, and output file!" << endl;
+        exit(1);
+    }
+
+    ifstream infile(argv[1]);
+    if (!infile.is_open()) {
+        cerr << "Unable to open file for reading: " << argv[1] << endl;
+        exit(2);
+    }
+
+    ofstream outfile(argv[2]);
+    if (!outfile.is_open()) {
+        cerr << "Unable to open file for writing: " << argv[2] << endl;
+        exit(3);
+    }
+
+    string line;
+    while (getline(infile, line)) {
+        // Process each line here
+        istringstream iss(line);
+        string word;
+        string first;
+        string last;
+        float points = 1;
+        int count = 0;
+
+        while (iss >> word) {
+            if (count >= 2) {
+                points *= stof(word);
+            }
+            else if (count == 0) {
+                first = word + ": ";
+            }
+            else {
+                last = word + ", ";
+            }
+            count ++;
+        }
+        outfile << last << first << points << endl;
+
+    }
+
+
+    infile.close();
+    outfile.close();
 
     return 0;
 }
