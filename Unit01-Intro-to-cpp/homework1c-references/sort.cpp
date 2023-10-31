61617b70acd46ebb1956dacb45f904ccc365f530
chilaust <118034353+chilaust@users.noreply.github.com>
2023-10-31 14:19:12 -0600

    Added sort.cpp I lost it somewhere and redownloaded off of gradescope

--- Unit01-Intro-to-cpp/homework1c-references/sort.cpp
+++ Unit01-Intro-to-cpp/homework1c-references/sort.cpp
@@ -1,8 +1,61 @@
 #include <iostream>
 #include <string>
 
+
 void sort(std::string& a, std::string& b, std::string& c) {
     // implement sort here
+    std::string A = a;
+    std::string B = b;
+    std::string C = c;
+    int Al = A.length();
+    int Bl = B.length();
+    int Cl = C.length();
+
+    if (a > b) {
+        std::swap(a, b);
+    }
+    if (a > c) {
+        std::swap(a, c);
+    }
+    if (b > c) {
+        std::swap(b, c);
+    }
+
+//    if (Al>Bl){
+//        if (Al>Cl) {
+//            a = A;
+//            if (Bl > Cl) {
+//                b = B;
+//                c = C;
+//            }
+//            else {
+//                b = C;
+//                c = B;
+//            }
+//        }
+//        else {
+//            a=C;
+//            b=A;
+//            c=B;
+//        }
+//    }
+//    else if (Bl>Cl){
+//        a=B;
+//        if (Al>Cl){
+//            b=A;
+//            c=C;
+//        }
+//        else{
+//            b=C;
+//            c=A;
+//        }
+//
+//    }
+//    else{
+//        a=C;
+//        b=B;
+//        c=A;
+//    }
 }
 
 int main(int argc, char const* argv[]) {
