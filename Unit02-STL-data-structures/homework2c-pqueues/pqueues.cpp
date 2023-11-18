5bdfde7c121cc8c3d2ec3f3cfbc083219a1a58e8
chilaust <118034353+chilaust@users.noreply.github.com>
2023-11-18 11:30:49 -0700

    final of VectorSet.h

--- Unit02-STL-data-structures/homework2c-pqueues/pqueues.cpp
+++ Unit02-STL-data-structures/homework2c-pqueues/pqueues.cpp
@@ -1,10 +1,72 @@
-#include <iostream>
 #include <string>
+using std::string, std::getline, std::stoi, std::stod;
+
+#include <sstream>
+using std::stringstream;
+
+#include <iostream>
+using std::cout, std::cin, std::cerr, std::endl;
+
+#include <stack>
+using std::stack;
+
+#include <queue>
+
+#include <iomanip>
 
 #include "input.h"
 
+#include <functional>
+
+std::string input(std::string prompt) {
+    std::cout << prompt;
+    std::cout.flush();
+    std::string response;
+    std::getline(std::cin, response);
+    return response;
+}
+
+std::string pad_priority (int number) {
+    std::ostringstream padded;
+    padded << std::setw(2) <<std::setfill('0') << number;
+    return padded.str();
+}
+
 int main(int argc, char const* argv[]) {
-    // Write your code here
+    // create que:
+    std::priority_queue<std::string, std::vector<std::string>, std::less<std::string> > people;
+
+
+    // start a loop so that it asks What do you want to do? (no endl)
+
+    while (true){
+        string operation = input("What do you want to do? ");
+        if (operation == "") {
+            break;
+        }
+
+        if (operation == "add") {
+            string name = input("Name: ");
+            string priority = pad_priority(stoi(input("Priority: ")));
+            //add name and priority to que...
+            people.push(priority + " - " + name);
+
+        }
+
+        else if (operation == "take") {
+            if (people.size() == 0) {
+                cout << "There are no more people in line" << endl;
+            }
+            else {
+                cout << people.top() << std::endl;
+                people.pop();
+            }
+        }
+        else {
+            cout << operation << " isn't a valid operation" << endl;
+        }
+
+    }
 
     return 0;
 }
